160f2849f1168fec0bfb215e00a43f0ade8c10bc
renatorrocha <renatorrodrigues2002@gmail.com>
2024-11-08 10:24:13 -0300

    feat: tailwind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+    integrations: [
+        react(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+    ],
+    vite: {
+        resolve: {
+            alias: {
+                "@": "/src",
+            },
+        },
+    },
 });
